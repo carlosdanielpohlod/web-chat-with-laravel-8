98045122593d39ce5d44a1e890dfdada7733f5e9
carlosdanielpohlod <carlospohlod@gmail.com>
2020-11-07 14:39:23 -0300

    Criado o controller de requisição externa com o método getAccessToken

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -18,13 +18,14 @@ class AuthController extends Controller
         $data = ['email' => $request['email'], 'password' => $request['password']];
         try{
             $token = $externalPost->getAuthToken(env('APP_API_URL')."/auth/login", $data);
+            
             $token = json_decode($token,true);
             echo $token['access_token'];
             // $this->tokenController->setAccessToken($token['access_token']);
-            // echo $this->tokenController->getAccessToken();
+            // $this->tokenController->getAccessToken();
         }
         catch(Exception $e){
-            // echo $e->getMessage();
+            echo $e->getMessage();
         }
     }
     
